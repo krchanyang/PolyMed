3ef67b6e7fe5b96793a1cd454e107783aba44f00
bellhyeon <bellhyeon@naver.com>
2023-05-08 13:34:45 +0900

    fix: Training model save paths

--- runners/training/train_ml.py
+++ runners/training/train_ml.py
@@ -25,123 +25,76 @@ class MLTrainingRunner:
         self.k = args.k
         self.seed = args.seed
         self.class_weights = args.class_weights
+        self.train_data_type = args.train_data_type
         self.save_base_path = os.path.join(args.save_base_path, args.train_data_type)
 
     def train(self):
         class_weights = compute_class_weights(self.train_y)
         sample_weights = compute_sample_weights(self.train_y)
-        if self.class_weights:
+        
+        model_save_path = os.path.join(self.save_base_path, "ML/baseline")
+        
+        if self.class_weights and self.train_data_type == "kb_extend":
             model_save_path = os.path.join(self.save_base_path, "ML/baseline/cw")
-            os.makedirs(model_save_path, exist_ok=True)
-            lr = LogisticRegression(random_state=self.seed, class_weight="balanced")
-            lr.fit(self.train_x, self.train_y)
-            joblib.dump(lr, os.path.join(model_save_path, "lr_cw.pkl"))
-
-            catboost = CatBoostClassifier(
-                task_type="GPU",
-                random_seed=self.seed,
-                verbose=False,
-                class_weights=class_weights,
-            )
-            catboost.fit(self.train_x, self.train_y)
-            catboost.save_model(os.path.join(model_save_path, "catboost_cw.pkl"))
-
-            lda = LinearDiscriminantAnalysis()
-            lda.fit(self.train_x, self.train_y)
-            joblib.dump(lda, os.path.join(model_save_path, "lda_cw.pkl"))
-
-            xgboost = XGBClassifier(seed=self.seed, use_label_encoder=False)
-            xgboost.fit(self.train_x, self.train_y, sample_weight=sample_weights)
-            joblib.dump(xgboost, os.path.join(model_save_path, "xgboost_cw.pkl"))
-
-            gbc = GradientBoostingClassifier(random_state=self.seed)
-            gbc.fit(self.train_x, self.train_y, sample_weight=sample_weights)
-            joblib.dump(gbc, os.path.join(model_save_path, "gb_cw.pkl"))
-
-            rf = RandomForestClassifier(
-                random_state=self.seed, n_jobs=-1, class_weight="balanced"
-            )
-            rf.fit(self.train_x, self.train_y)
-            joblib.dump(rf, os.path.join(model_save_path, "rf_cw.pkl"))
-
-            et = ExtraTreesClassifier(
-                random_state=self.seed, n_jobs=-1, class_weight="balanced"
-            )
-            et.fit(self.train_x, self.train_y)
-            joblib.dump(et, os.path.join(model_save_path, "et_cw.pkl"))
-
-            dt = DecisionTreeClassifier(random_state=self.seed, class_weight="balanced")
-            dt.fit(self.train_x, self.train_y)
-            joblib.dump(dt, os.path.join(model_save_path, "dt_cw.pkl"))
-
-            knn = KNeighborsClassifier(n_jobs=-1)
-            knn.fit(self.train_x, self.train_y)
-            joblib.dump(knn, os.path.join(model_save_path, "knn_cw.pkl"))
-
-            nb = GaussianNB()
-            nb.fit(self.train_x, self.train_y, sample_weight=sample_weights)
-            joblib.dump(nb, os.path.join(model_save_path, "nb_cw.pkl"))
-
-            ada = AdaBoostClassifier(random_state=self.seed)
-            ada.fit(self.train_x, self.train_y, sample_weight=sample_weights)
-            joblib.dump(ada, os.path.join(model_save_path, "adaboost_cw.pkl"))
-
-            lgbm = LGBMClassifier(
-                random_state=self.seed, n_jobs=-1, class_weight="balanced"
-            )
-            lgbm.fit(self.train_x, self.train_y)
-            joblib.dump(lgbm, os.path.join(model_save_path, "lgbm_cw.pkl"))
-        else:
+        if not self.class_weights and self.train_data_type == "kb_extend":
             model_save_path = os.path.join(self.save_base_path, "ML/baseline/ncw")
-            os.makedirs(model_save_path, exist_ok=True)
-            lr = LogisticRegression(random_state=self.seed)
-            lr.fit(self.train_x, self.train_y)
-            joblib.dump(lr, os.path.join(model_save_path, "lr.pkl"))
+        
+        os.makedirs(model_save_path, exist_ok=True)
+        lr = LogisticRegression(random_state=self.seed, class_weight="balanced")
+        lr.fit(self.train_x, self.train_y)
+        joblib.dump(lr, os.path.join(model_save_path, "lr.pkl"))
 
-            catboost = CatBoostClassifier(
-                task_type="GPU",
-                random_seed=self.seed,
-                verbose=False,
-            )
-            catboost.fit(self.train_x, self.train_y)
-            catboost.save_model(os.path.join(model_save_path, "catboost.pkl"))
+        catboost = CatBoostClassifier(
+            task_type="GPU",
+            random_seed=self.seed,
+            verbose=False,
+            class_weights=class_weights,
+        )
+        catboost.fit(self.train_x, self.train_y)
+        catboost.save_model(os.path.join(model_save_path, "catboost.pkl"))
 
-            lda = LinearDiscriminantAnalysis()
-            lda.fit(self.train_x, self.train_y)
-            joblib.dump(lda, os.path.join(model_save_path, "lda.pkl"))
+        lda = LinearDiscriminantAnalysis()
+        lda.fit(self.train_x, self.train_y)
+        joblib.dump(lda, os.path.join(model_save_path, "lda.pkl"))
 
-            xgboost = XGBClassifier(seed=self.seed, use_label_encoder=False)
-            xgboost.fit(self.train_x, self.train_y)
-            joblib.dump(xgboost, os.path.join(model_save_path, "xgboost.pkl"))
+        xgboost = XGBClassifier(seed=self.seed, use_label_encoder=False)
+        xgboost.fit(self.train_x, self.train_y, sample_weight=sample_weights)
+        joblib.dump(xgboost, os.path.join(model_save_path, "xgboost.pkl"))
 
-            gbc = GradientBoostingClassifier(random_state=self.seed)
-            gbc.fit(self.train_x, self.train_y)
-            joblib.dump(gbc, os.path.join(model_save_path, "gb.pkl"))
+        gbc = GradientBoostingClassifier(random_state=self.seed)
+        gbc.fit(self.train_x, self.train_y, sample_weight=sample_weights)
+        joblib.dump(gbc, os.path.join(model_save_path, "gb.pkl"))
 
-            rf = RandomForestClassifier(random_state=self.seed, n_jobs=-1)
-            rf.fit(self.train_x, self.train_y)
-            joblib.dump(rf, os.path.join(model_save_path, "rf.pkl"))
+        rf = RandomForestClassifier(
+            random_state=self.seed, n_jobs=-1, class_weight="balanced"
+        )
+        rf.fit(self.train_x, self.train_y)
+        joblib.dump(rf, os.path.join(model_save_path, "rf.pkl"))
 
-            et = ExtraTreesClassifier(random_state=self.seed, n_jobs=-1)
-            et.fit(self.train_x, self.train_y)
-            joblib.dump(et, os.path.join(model_save_path, "et.pkl"))
+        et = ExtraTreesClassifier(
+            random_state=self.seed, n_jobs=-1, class_weight="balanced"
+        )
+        et.fit(self.train_x, self.train_y)
+        joblib.dump(et, os.path.join(model_save_path, "et.pkl"))
 
-            dt = DecisionTreeClassifier(random_state=self.seed)
-            dt.fit(self.train_x, self.train_y)
-            joblib.dump(dt, os.path.join(model_save_path, "dt.pkl"))
+        dt = DecisionTreeClassifier(random_state=self.seed, class_weight="balanced")
+        dt.fit(self.train_x, self.train_y)
+        joblib.dump(dt, os.path.join(model_save_path, "dt.pkl"))
 
-            knn = KNeighborsClassifier(n_jobs=-1)
-            knn.fit(self.train_x, self.train_y)
-            joblib.dump(knn, os.path.join(model_save_path, "knn.pkl"))
+        knn = KNeighborsClassifier(n_jobs=-1)
+        knn.fit(self.train_x, self.train_y)
+        joblib.dump(knn, os.path.join(model_save_path, "knn.pkl"))
 
-            nb = GaussianNB()
-            nb.fit(self.train_x, self.train_y)
-            joblib.dump(nb, os.path.join(model_save_path, "nb.pkl"))
+        nb = GaussianNB()
+        nb.fit(self.train_x, self.train_y, sample_weight=sample_weights)
+        joblib.dump(nb, os.path.join(model_save_path, "nb.pkl"))
 
-            ada = AdaBoostClassifier(random_state=self.seed)
-            ada.fit(self.train_x, self.train_y)
-            joblib.dump(ada, os.path.join(model_save_path, "adaboost.pkl"))
+        ada = AdaBoostClassifier(random_state=self.seed)
+        ada.fit(self.train_x, self.train_y, sample_weight=sample_weights)
+        joblib.dump(ada, os.path.join(model_save_path, "adaboost.pkl"))
 
-            lgbm = LGBMClassifier(random_state=self.seed, n_jobs=-1)
-            lgbm.fit(self.train_x, self.train_y)
-            joblib.dump(lgbm, os.path.join(model_save_path, "lgbm.pkl"))
+        lgbm = LGBMClassifier(
+            random_state=self.seed, n_jobs=-1, class_weight="balanced"
+        )
+        lgbm.fit(self.train_x, self.train_y)
+        joblib.dump(lgbm, os.path.join(model_save_path, "lgbm.pkl"))
--- utils/constants.py
+++ utils/constants.py
@@ -13,7 +13,6 @@ ML_MODEL_DICT = {
     "lgbm": "LGBMClassifier",
 }
 
-
 NORM_ML_MODEL_SAVE_PATH = "./experiments/norm/ML/baseline/*.pkl"
 EXTEND_ML_MODEL_SAVE_PATH = "./experiments/extend/ML/baseline/*.pkl"
 KB_EXTEND_ML_MODEL_SAVE_PATH = "./experiments/kb_extend/ML/baseline/ncw/*.pkl"
@@ -21,10 +20,11 @@ KB_EXTEND_CLASS_WEIGHTS_ML_MODEL_SAVE_PATH = (
     "./experiments/kb_extend/ML/baseline/cw/*.pkl"
 )
 
-
 EXTEND_TUNED_ML_MODEL_SAVE_PATH = "./experiments/extend/ML/tuned/*.pkl"
-KB_EXTEND_TUNED_ML_MODEL_SAVE_PATH = "./experiments/kb_extend/ML/tuned/*.pkl"
+KB_EXTEND_TUNED_ML_MODEL_SAVE_PATH = "./experiments/kb_extend/ML/tuned/ncw/*.pkl"
+KB_EXTEND_TUNED_CLASS_WEIGHTS_ML_MODEL_SAVE_PATH = "./experiments/kb_extend/ML/tuned/cw/*.pkl"
 NORM_TUNED_ML_MODEL_SAVE_PATH = "./experiments/norm/ML/tuned/*.pkl"
+
 
 NORM_MLP_MODEL_SAVE_PATH = "./experiments/norm/MLP"
 EXTEND_MLP_MODEL_SAVE_PATH = "./experiments/extend/MLP"
@@ -43,11 +43,3 @@ GRAPH_V1_SAVED_MODEL_NAME = "knowledge_mlp_v1.pt"
 EXTEND_GRAPH_V2_MODEL_SAVE_PATH = "./experiments/extend/Graph_v2"
 KB_EXTEND_GRAPH_V2_MODEL_SAVE_PATH = "./experiments/kb_extend/Graph_v2"
 GRAPH_V2_SAVED_MODEL_NAME = "knowledge_mlp_v2.pt"
-
-# NORM_ML_MODEL_SAVE_PATH = "./weights/norm/ML/baseline/*.pkl"
-# EXTEND_ML_MODEL_SAVE_PATH = "./weights/extend/ML/baseline/*.pkl"
-# KB_EXTEND_ML_MODEL_SAVE_PATH = "./weights/kb_extend/ML/baseline/*.pkl"
-
-# EXTEND_TUNED_ML_MODEL_SAVE_PATH = "./weights/extend/*.pkl"
-# KB_EXTEND_TUNED_ML_MODEL_SAVE_PATH = "./weights/kb_extend(ncw)/*.pkl"
-# NORM_TUNED_ML_MODEL_SAVE_PATH = "./weights/norm/*.pkl"
